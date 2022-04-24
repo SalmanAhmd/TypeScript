48f39ef28469fe58c1ad71d42c110247b9d93fd4
SalmanAhmd <salmanahmed1497@gmail.com>
2022-04-25 03:26:26 +0530

    object

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
 import Greet from './components/Greet';
+import Person from './components/Person';
 
 function App() {
   return (
     <div className="App">
-      <Greet name='Salman' />
+      <Greet messageCount={32} name='Salman' isLoggedIn />
+      <Person name={{ first: 'Salman', last: 'Ahmed' }} />
     </div>
   );
 }
--- src/components/Greet.tsx
+++ src/components/Greet.tsx
@@ -1,9 +1,18 @@
 type Props = {
-  name: string
+  name: string,
+  messageCount: number,
+  isLoggedIn?: boolean // ? optional
 }
 
 export default function Greet(props: Props) {
   return (
-    <div>Welcome {props.name}</div>
+    <div>
+      <h2>
+        Welcome {props.name}
+      </h2>
+      {props.isLoggedIn && <h3>
+        You Have {props.messageCount}!
+      </h3>}
+    </div>
   )
 }
