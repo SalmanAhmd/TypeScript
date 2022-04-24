c6fd4f35225e5818184b585836d7bcce82a786df
SalmanAhmd <salmanahmed1497@gmail.com>
2022-04-25 03:30:11 +0530

    array of object

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css';
 import Greet from './components/Greet';
 import Person from './components/Person';
+import PersonList from './components/PersonList';
 
 function App() {
   return (
     <div className="App">
       <Greet messageCount={32} name='Salman' isLoggedIn />
       <Person name={{ first: 'Salman', last: 'Ahmed' }} />
+      <PersonList names={[{ first: 'Salman', last: 'Ahmed' }, { first: 'Kamaal', last: 'Malik' }]} />
     </div>
   );
 }
