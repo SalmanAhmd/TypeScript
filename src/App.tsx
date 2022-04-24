1b76efa482fe24a6eb3a99f57dc9b86d66e0cdbd
SalmanAhmd <salmanahmed1497@gmail.com>
2022-04-25 03:39:24 +0530

    children string

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css';
 import Greet from './components/Greet';
+import Heading from './components/Heading';
 import Person from './components/Person';
 import PersonList from './components/PersonList';
 import Status from './components/Status';
@@ -12,6 +13,8 @@ function App() {
       <PersonList names={[{ first: 'Salman', last: 'Ahmed' }, { first: 'Kamaal', last: 'Malik' }]} />
 
       <Status status='loading' />
+
+      <Heading>Heading Text </Heading>
     </div>
   );
 }
